a31aeccc1638110ab202efbe9e4f6e128f78637f
Leah-Tugi <leahsparkle1@gmail.com>
2023-04-25 17:02:40 +0300

    calculate active flags calculate precision for printing calculate size to cast argument calculate width for printing print argument based on its type evaluate if character is printable write handlers

---
+++ 5-get_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_n = *i + 1;
+	int precision = -1;
+
+	if (format[curr_n] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_n += 1; format[curr_n] != '\0'; curr_n++)
+	{
+		if (is_digit(format[curr_n]))
+		{
+			precision *= 10;
+			precision += format[curr_n] - '0';
+		}
+		else if (format[curr_n] == '*')
+		{
+			curr_n++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_n - 1;
+
+	return (precision);
+}
+
